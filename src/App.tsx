d345d936c67edb1b4de888cabb1875528eb9d3c3
nogari03 <mmgg555@naver.com>
2024-03-03 15:14:59 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { ThemeProvider } from "@emotion/react";
-import { ConnectedRouter } from "connected-react-router";
-import { BrowserHistory } from "history";
-import { Provider } from "react-redux";
-import { Store } from "redux";
-import LayoutContainer from "./containers/LayoutContainer";
-import Routes from "./routes";
-import { ApplicationState } from "./store";
-import * as themes from "./styles/theme";
 
-interface MainProps {
-  store: Store<ApplicationState>
-  history: BrowserHistory
-}
-
-const App: React.FC<MainProps> = ({ store, history }) => {
+const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <LayoutContainer>
-          {({ theme }) => (
-            <ThemeProvider theme={themes[theme]}>
-              <Routes />
-            </ThemeProvider>
-          )}
-        </LayoutContainer>
-      </ConnectedRouter>
-    </Provider>
+    <>
+    TEST
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,18 @@
-import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import configureStore from './configureStore';
 import reportWebVitals from './reportWebVitals';
-import { initialState } from './store/layout';
-
-import 'typeface-ibm-plex-sans';
-
-const history = createBrowserHistory();
-const store = configureStore(history, initialState);
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App store={store} history={history} />
+    <App />
   </React.StrictMode>
 );
 
 reportWebVitals();
+serviceWorker.unregister();
